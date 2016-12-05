fc595c1c7dd6f28d388095f418be4c84e96d629c
sandrew1945 <sandrew1945@gmail.com>
2016-12-05 11:00:01 +0800

    1、修复model类名、属性名如不按照默认生成规则增删改查出错的BUG

--- src/main/java/org/apache/ibatis/callback/POCallBack.java
+++ src/main/java/org/apache/ibatis/callback/POCallBack.java
@@ -55,7 +55,9 @@ public class POCallBack<T extends Object> implements DAOCallback<T>
 			for (int i = 0; i < mapping.getColSize(); i++)
 			{
 				Object value = POUtil.getValue(rs, mapping.getColName(i), mapping.getColType(i));
-				POUtil.invokeSetMethodByField(po, POUtil.getAttributeNameByFieldName(mapping.getColName(i)), value);
+				// modi by weibin 解决model类名不符合默认生成规则时无法正确返回结果的BUG
+				//POUtil.invokeSetMethodByField(po, POUtil.getAttributeNameByFieldName(mapping.getColName(i)), value);
+				POUtil.invokeSetMethodByField(po, mapping.getPropertyName(i), value);
 			}
 			return po;
 		}
